da034f04a30603d664f7b06ac51c8b826286cb8f
Drmrsan <drmrsan@gmail.com>
2016-09-06 01:05:28 +0200

    Tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,22 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true
 	has_many :posts, dependent: :destroy
-	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id",
-						dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+
+	has_many :following, through: :active_relationships, source: followed
+	has_many :followers, through: :passive_relationships, source: follower
+
+	def follow(other)
+		active_relationships.create(followed_id: other.id)
+	end
+
+	def unfollow(other)
+		active_relationships.destroy(followed_id: other.id).destroy
+	end
+
+	def following?(other)
+		following.include?(other)		
+	end
 end
