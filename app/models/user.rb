e3573448196f1415de54f41059c9c081c7316e49
Drmrsan <drmrsan@gmail.com>
2016-09-05 15:09:23 +0000

    Relationship model, part1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-	validates :username, presence: true
-	has_many :posts
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username, presence: true
+	has_many :posts, dependent: :destroy
+	has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id",
+						dependent: :destroy
 end
